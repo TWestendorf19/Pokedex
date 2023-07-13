47fc4a823c9b63066b51fb3ddb2db9ede44f73c3
twestendorf <tyson.westendorf@logan-aluminum.com>
2023-07-13 14:47:55 -0500

    Pushing to access progress at home

--- command_map.go
+++ command_map.go
@@ -5,11 +5,41 @@ import (
 )
 
 func commandMap(cf *config) error {
-	// print out location in area and increment area
+	// get location response
+	locationsResp, err := cf.pokeAPIClient.ListLocations(cf.nextURL)
+	if err != nil {
+		return err
+	}
+
+	// reassign next and previous location URLs
+	cf.nextURL = locationsResp.Next
+	cf.previousURL = locationsResp.Previous
+
+	// print out all locations in area
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+
 	return nil
 }
 
-func commandMapSpec(cf *config, area string) error {
-	fmt.Printf("Printing out all locations in area: '%s'...", area)
+func commandMapSpec(cf *config, location string) error {
+	fmt.Printf("Printing out all areas in location: '%s'...", location)
+	*cf.currentURL = "https://pokeapi.co/api/v2/location-area/" + location
+	// get location response
+	locationsResp, err := cf.pokeAPIClient.ListLocations(cf.currentURL)
+	if err != nil {
+		return err
+	}
+
+	// reassign next and previous location URLs
+	cf.nextURL = locationsResp.Next
+	cf.previousURL = locationsResp.Previous
+
+	// print out all locations in area
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
+
 	return nil
 }
--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -3,8 +3,15 @@ package pokeapi
 type RespShallowLocations struct {
 	Count    int     `json:"count"`
 	Next     *string `json:"next"`
-	Previous *string `json:"Previous"`
+	Previous *string `json:"previous"`
 	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type RespAreaLocations struct {
+	Results []struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
